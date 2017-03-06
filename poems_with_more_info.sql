1458db24e4c3813d0eb60adc3b7cdc9ef9193349
Alireza <a_ghasemi@outlook.com>
2017-03-06 12:55:32 +0100

    Dataset with book and poem title added

---
+++ poems_with_more_info.sql
@@ -0,0 +1,6 @@
+SELECT poet.name AS poet, century, cat.text as book_title, poem.title AS poem_title, TRIM(GROUP_CONCAT(verse.text , '    ')) as poem
+  FROM verse,poem,cat,poet
+  WHERE verse.poem_id=poem.id AND poem.cat_id=cat.id AND cat.poet_id=poet.id
+  GROUP BY poem_id
+  HAVING poem NOT LIKE '%'||CHAR(10)||'%' AND poem NOT LIKE '%'||CHAR(13)||'%'
+  ORDER BY century
