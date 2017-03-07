a85bc05a679c999161d8e0bd6d1242cf502fb60f
Alireza Ghasemi <ghasemi.a.ir@gmail.com>
2017-03-07 10:33:49 +0100

    Update verses.sql

--- verses.sql
+++ verses.sql
@@ -1,4 +1,4 @@
-SELECT poet.name as poet, century,  verse.text as verse
+SELECT poet.name AS poet, century,  verse.text AS verse
   FROM verse,poem,cat,poet
   WHERE verse.poem_id=poem.id AND poem.cat_id=cat.id AND cat.poet_id=poet.id 
   AND verse NOT LIKE '%'||CHAR(10)||'%' AND verse NOT LIKE '%'||CHAR(13)||'%'
