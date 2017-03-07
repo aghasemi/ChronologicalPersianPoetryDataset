9987bcffc5bbe5aa828489068f8a83d6c5ecc088
Alireza Ghasemi <ghasemi.a.ir@gmail.com>
2017-03-07 10:33:18 +0100

    Update poems.sql

--- poems.sql
+++ poems.sql
@@ -1,4 +1,4 @@
-SELECT poet.name as poet, century, TRIM(GROUP_CONCAT(verse.text , '    ')) as poem
+SELECT poet.name AS poet, century, TRIM(GROUP_CONCAT(verse.text , '    ')) AS poem
   FROM verse,poem,cat,poet
   WHERE verse.poem_id=poem.id AND poem.cat_id=cat.id AND cat.poet_id=poet.id
   GROUP BY poem_id
